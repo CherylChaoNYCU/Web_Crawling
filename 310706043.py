5e3b7e12b2fa936639991a62b65e556a2c9ab356
CherylChao <cherry885930@yahoo.com.tw>
2023-02-24 10:30:57 +0800

    update the date edge case

--- 310706043.py
+++ 310706043.py
@@ -338,20 +338,34 @@ if __name__ == '__main__':
             with open('all_popular.jsonl','r') as f:
                 data = json.load(f)
             
-            srt_d = argv[2]
-            end_d = argv[3]
+            srt_d = int(argv[2])
+            end_d = int(argv[3])
             srtidx = endidx = 0
-
-            for i in range(len(data)):
-                if((data[i]['date: '] == srt_d) and srtidx == 0):
+            i = 0
+           
+            
+           
+            while(i <  len(data)):
+                cur_date = int(data[i]['date: '])
+                if((cur_date == srt_d) and srtidx == 0):
                     srtidx = i
-                if(data[i]['date: '] == end_d):
-                    while(data[i]['date: '] == end_d):
+                    
+                elif(cur_date == end_d):
+                    while(int(data[i]['date: ']) == end_d):
                         i+=1
                     endidx = i
                     break
+                elif(cur_date > end_d): #no end date found
+                    endidx = i
+                    break
+                i+=1
+
+           
+  
+
             #number of popular articles:
             num = (endidx - srtidx)
+
             
             for idx in range(srtidx,endidx):
 
--- test.jsonformat.py
+++ test.jsonformat.py
@@ -1,17 +1,22 @@
-import json
+# import json
 
-with open('all_article.jsonl','r') as f:
-    data = json.load(f)
+# with open('all_article.jsonl','r') as f:
+#     data = json.load(f)
 
-for i in range(20):
-    print(data[i])
-    if(data[i]['date: '] == '0101'):
-        print(data[i]['date: '])
-dict = {'gtoamdk7': 2, 'Swando': 1, 'ninimikoo': 1, 'Kelite': 1, 'w2884939': 1, 'Vek1112': 1, 'Eligor41': 1, 'ptt821105': 1, 'SCurry30': 1, 'diminifish': 1}
-items = dict.items()
-keys = []
-values = []
-for item in items:
-    keys.append(item[0]), values.append(item[1])
-print(keys[0])
-print(values)
+# for i in range(20):
+#     print(data[i])
+#     if(data[i]['date: '] == '0101'):
+#         print(data[i]['date: '])
+# dict = {'gtoamdk7': 2, 'Swando': 1, 'ninimikoo': 1, 'Kelite': 1, 'w2884939': 1, 'Vek1112': 1, 'Eligor41': 1, 'ptt821105': 1, 'SCurry30': 1, 'diminifish': 1}
+# items = dict.items()
+# keys = []
+# values = []
+# for item in items:
+#     keys.append(item[0]), values.append(item[1])
+# print(keys[0])
+# print(values)
+
+a = '0203'
+b = '0511'
+
+print(int(a) - int(b))
